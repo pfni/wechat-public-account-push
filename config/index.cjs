015a84fbb7a32bc16206414e1c85abe1b0529edf
pfni <131629657+pfni@users.noreply.github.com>
2024-10-12 15:18:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'dn2018year', 'Aug-N14'
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU32416TmUQWp3BiDgcJzO9zhVWoKJto4nBQdQeF',
+      id: 'PDU32416TrpR2osZe5qgd67VxMAaUNQD2WRzr2yAz',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
