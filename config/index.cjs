39f79b527f5bde83b13bf407ff040abfacf2859c
pfni <131629657+pfni@users.noreply.github.com>
2024-10-12 15:36:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,7 +20,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: 'dn2018year', 'Aug-N14',
+      name: 'dn2018year',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU32416TrpR2osZe5qgd67VxMAaUNQD2WRzr2yAz',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
