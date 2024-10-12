11703498a804b4ea64f598e7eb89db1986a3aa42
pfni <131629657+pfni@users.noreply.github.com>
2024-10-12 14:51:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,7 +20,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: 'dttnpy',
+      name: 'dn2018year', 'Aug-N14'
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU32416TmUQWp3BiDgcJzO9zhVWoKJto4nBQdQeF',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -56,7 +56,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: 'dttnpy',
+      name: 'dn2018year',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '6e3b01b4b9694ee928d0a1f481ba13ac',
     }
